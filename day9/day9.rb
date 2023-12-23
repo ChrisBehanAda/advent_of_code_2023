1c2b3b491372b08c90030cb0ff7046d77f7e4090
Chris Behan <chris.behan@ada.support>
2023-12-22 17:00:55 -0700

    Day 9 part 2

--- day9/day9.rb
+++ day9/day9.rb
@@ -10,21 +10,21 @@ def extract_histories(input)
   input.split("\n").map { |h| h.strip.split(/\s+/) }
 end
 
-def extrapolate(history)
+def calculate_diffs(history)
   diff_list = [history]
   while !diff_list[-1].all? { |d| d == 0}
-    diffs = []
-    lastest_diff = diff_list[-1]
-    (0...lastest_diff.length() -1).each do |i|
-      diffs << lastest_diff[i+1].to_i() - lastest_diff[i].to_i()
-    end
+    latest_diff = diff_list[-1]
+    diffs = (0...latest_diff.length() -1).map { |i| latest_diff[i+1].to_i() - latest_diff[i].to_i()}
     diff_list << diffs
   end
+  return diff_list
+end
 
+def extrapolate(history)
+  diff_list = calculate_diffs(history)
   next_val = 0
   (diff_list.length()-1).downto(0) do |i|
     val = diff_list[i][-1].to_i()
-    # puts val
     next_val += val
   end
   return next_val
@@ -37,6 +37,24 @@ def part1()
   puts "Part 1 answer: #{total}"
 end
 
+def extrapolate_backwards(history)
+  diff_list = calculate_diffs(history)
+  diff_list[-1].unshift(0)
+  (diff_list.length()-1).downto(1).each do |i|
+    extrapolated_val = diff_list[i-1][0].to_i() - diff_list[i][0].to_i()
+    diff_list[i-1].unshift(extrapolated_val)
+  end
+  return diff_list[0][0]
+end
+
+def part2()
+  input = get_input("input.txt")
+  histories = extract_histories(input)
+  total = histories.reduce(0) { |sum, h| sum + extrapolate_backwards(h) }
+  puts "Part 2 answer: #{total}"
+end
+
 
 
 part1()
+part2()
